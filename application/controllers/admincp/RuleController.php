ef114476ed4aaab406bd93e3f4c21b1ab48b3f11
Bathe0603it <32597011+Bathe0603it@users.noreply.github.com>
2017-10-14 12:25:55 +0700

    new Rule
    add

---
+++ application/controllers/admincp/RuleController.php
@@ -0,0 +1,50 @@
+<?php
+    defined('BASEPATH') or exit('No direct script access allowed');
+    /**
+     * 
+     * function action login admin
+     * 
+     * */
+    class RuleController extends MY_Controller
+    {
+        public function __construct(){
+            parent::__construct();
+            $this->load->model('rulemodel');
+        }
+    
+        public function index(){
+            $getall = $this->usermodel->get_all();
+            $data['getall'] = $getall;
+            $this->loadview($this->view,$data);
+        }
+        
+        public function edit(){
+            
+        }
+        
+        public function create(){
+            if (is_post()) {
+                $this->postCreate();
+            }
+            $this->loadview($this->view);
+        }
+        private function postCreate(){
+            // xu ly formvalidate
+            $input  = $this->input->post();
+            if ($this->form_validation->run('user_create')) {
+                $arr_insert = $input;
+                // quy doi password luu kieu md5
+                $arr_insert['password'] = md5($this->auth->suffix_pass.$input['password']);
+                $this->usermodel->insert($arr_insert);
+            }
+            else{
+                
+            }
+            
+            // neu ok
+            
+            // neu k ok
+        }
+        
+        
+    }
