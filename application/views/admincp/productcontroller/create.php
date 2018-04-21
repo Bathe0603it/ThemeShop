40d44c00758d39f25bf4b950de9a262474b01948
bathe0603it <bathe0603it@gmail.com>
2018-04-21 07:56:58 +0700

    add home
    `

--- application/views/admincp/productcontroller/create.php
+++ application/views/admincp/productcontroller/create.php
@@ -78,7 +78,7 @@
                 <div class="col-md-12">
                     <div class="box box-warning">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Giá sản phẩm</h3>
+                            <h3 class="box-title">Kho hàng</h3>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
@@ -86,23 +86,75 @@
                             <div class="row">
                                 <div class="col-md-6 col-xs-12">
                                     <div class="form-group">
-                                        <label>Giá gốc</label>
-                                        <input type="text" class="form-control" placeholder="Enter ...">
+                                        <label for="">Mã sản phẩm / SKU</label>
+                                        <input type="code" class="form-control" placeholder="Enter ...">
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-xs-12">
                                     <div class="form-group">
-                                        <label>Giá khuyến mãi</label>
+                                        <label>Mã vạch / Barcode (ISBN, UPC, v.v...)</label>
                                         <input type="text" class="form-control" placeholder="Enter ...">
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-6 col-xs-12">
-                                    <label>
-                                        <input type="checkbox" class="flat-red" checked>
-                                        Giá đã bao gồm VAT
-                                    </label>
+                                    <div class="form-group">
+                                        <label for="">Quản lý kho</label>
+                                        <select class="form-control">
+                                            <option value="">Không quản lý kho hàng</option>
+                                            <option value="">Quản lý kho hàng</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group">
+                                        <label for="">Số lượng</label>
+                                        <input type="text" class="form-control" placeholder="Enter ...">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="box box-warning">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Xem trước kết quả tìm kiếm (Tùy chỉnh SEO)</h3>
+                            
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div>
+                                <p>Xin hãy nhập Tiêu đề và Mô tả để xem trước kết quả tìm kiếm của sản phẩm này.</p>
+                            </div>
+                            <!-- text input -->
+                            <div class="row">
+                                <div class="col-md-12 col-xs-12">
+                                    <div class="form-group">
+                                        <p><label for="">Thẻ tiêu đề</label><span class="text-right">1/70</span></p>
+                                        <input type="code" class="form-control" placeholder="Enter ...">
+                                    </div>
+                                </div>
+                                <div class="col-md-12 col-xs-12">
+                                    <div class="form-group">
+                                        <p><label for="">Thẻ mô tả</label><span class="text-right">1/320</span></p>
+                                        <textarea class="form-control"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 col-xs-12">
+                                    <div class="form-group">
+                                        <label for="">Đường dẫn / Alias</label>
+                                        <div class="input-group">
+                                            <span class="input-group-addon">http://batheweb/</span>
+                                            <input type="text" class="form-control" placeholder="Username">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -112,7 +164,30 @@
             </div>
         </div>
         <div class="col-md-4 col-xs-12">
-            
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Trạng thái</h3>
+                        </div>
+                        <div class="box-body">
+                            <div class="form-group">
+                                
+                                <input type="radio" name="status" id="hidden" placeholder="Enter name product"><label for="hidden">Ẩn</label>
+                            </div>
+                            <div class="form-group">
+                                
+                                <input type="radio" name="status" id="display" placeholder="Enter name product"><label for="display">Hiện</label>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Thời gian hiển thị</label>
+                                <input type="text" class="form-control" placeholder="Username">
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     
